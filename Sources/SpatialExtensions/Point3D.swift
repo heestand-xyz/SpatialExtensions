efa223c403365d098aa2312e2782c2b7dd005a59
Anton Heestand <anton.heestand@gmail.com>
2024-02-11 20:36:51 +0900

    3D Nodes

--- Sources/SpatialExtensions/Point3D.swift
+++ Sources/SpatialExtensions/Point3D.swift
@@ -1,4 +1,5 @@
 import Spatial
+import simd
 
 public extension Point3D {
     
@@ -6,5 +7,21 @@ public extension Point3D {
         Point3D(x: lhs.x + rhs.x, 
                 y: lhs.y + rhs.y,
                 z: lhs.z + rhs.z)
+    }
+}
+
+public extension Point3D {
+    
+    var simd3: SIMD3<Float> {
+        SIMD3<Float>(x: Float(x),
+                     y: Float(y),
+                     z: Float(z))
+    }
+}
+
+public extension SIMD3<Float> {
+    
+    var point3D: Point3D {
+        Point3D(self)
     }
 }
