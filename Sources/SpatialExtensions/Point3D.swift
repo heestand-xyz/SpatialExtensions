0cdb9a697fc1dcedb862f98b6e4cb34efff2ff89
Anton Heestand <anton.heestand@gmail.com>
2024-02-12 17:05:38 +0900

    Sphere

--- Sources/SpatialExtensions/Point3D.swift
+++ Sources/SpatialExtensions/Point3D.swift
@@ -8,6 +8,12 @@ public extension Point3D {
                 y: lhs.y + rhs.y,
                 z: lhs.z + rhs.z)
     }
+    
+    static func - (lhs: Point3D, rhs: Point3D) -> Point3D {
+        Point3D(x: lhs.x - rhs.x, 
+                y: lhs.y - rhs.y,
+                z: lhs.z - rhs.z)
+    }
 }
 
 public extension Point3D {
