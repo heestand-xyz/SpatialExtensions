4db85263fd1be61d09c778ffc990a6fcce98a364
Anton Heestand <anton.heestand@gmail.com>
2024-02-18 11:55:53 +0900

    Wiring

---
+++ Sources/SpatialExtensions/Vector3D.swift
@@ -0,0 +1,18 @@
+import Spatial
+import simd
+
+public extension Vector3D {
+    
+    var simd3: SIMD3<Float> {
+        SIMD3<Float>(x: Float(x),
+                     y: Float(y),
+                     z: Float(z))
+    }
+}
+
+public extension SIMD3<Float> {
+    
+    var vector3D: Vector3D {
+        Vector3D(self)
+    }
+}
