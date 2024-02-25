09a3ab8ecf68dbb93d1c9f2049833e61e57dbf4d
Heestand, Anton Norman | Anton | GSSD <antonnorman.heestand@rakuten.com>
2024-02-25 19:57:40 +0900

    Geo Selection

--- Sources/SpatialExtensions/Size3D.swift
+++ Sources/SpatialExtensions/Size3D.swift
@@ -5,6 +5,15 @@ public extension Size3D {
     static let one = Size3D(width: 1.0, height: 1.0, depth: 1.0)
 }
 
+public extension Size3D {
+    
+    var simd3: SIMD3<Float> {
+        SIMD3<Float>(x: Float(width),
+                     y: Float(height),
+                     z: Float(depth))
+    }
+}
+
 public extension Point3D {
     
     var size3D: Size3D {
