12ce2dcdd950e6eb79a72244fb67f05ba34352a9
Heestand, Anton Norman | Anton | GSSD <antonnorman.heestand@rakuten.com>
2024-03-15 10:39:55 +0900

    Spatial Types

--- Sources/SpatialExtensions/Size3D.swift
+++ Sources/SpatialExtensions/Size3D.swift
@@ -2,11 +2,6 @@ import Spatial
 
 public extension Size3D {
     
-    static let one = Size3D(width: 1.0, height: 1.0, depth: 1.0)
-}
-
-public extension Size3D {
-    
     var simd3: SIMD3<Float> {
         SIMD3<Float>(x: Float(width),
                      y: Float(height),
