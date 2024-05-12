2cab88a72d3c83de78fe04ad707ac6c7cd80edf4
Anton Heestand <anton.heestand@gmail.com>
2024-05-12 20:55:18 +0900

    Fix Position of Selection Shapes

--- Sources/SpatialExtensions/Point3D.swift
+++ Sources/SpatialExtensions/Point3D.swift
@@ -39,18 +39,45 @@ public extension Point3D {
                 z: lhs.z + rhs.z)
     }
     
+    static func += (lhs: inout Point3D, rhs: Point3D) {
+        lhs = Point3D(x: lhs.x + rhs.x,
+                      y: lhs.y + rhs.y,
+                      z: lhs.z + rhs.z)
+    }
+    
     static func - (lhs: Point3D, rhs: Point3D) -> Point3D {
         Point3D(x: lhs.x - rhs.x,
                 y: lhs.y - rhs.y,
                 z: lhs.z - rhs.z)
     }
-}
-
-public extension Point3D {
+    
+    static func -= (lhs: inout Point3D, rhs: Point3D) {
+        lhs = Point3D(x: lhs.x - rhs.x,
+                      y: lhs.y - rhs.y,
+                      z: lhs.z - rhs.z)
+    }
+    
+    static func * (lhs: Point3D, rhs: Size3D) -> Point3D {
+        Point3D(x: lhs.x * rhs.width,
+                y: lhs.y * rhs.height,
+                z: lhs.z * rhs.depth)
+    }
+    
+    static func *= (lhs: inout Point3D, rhs: Point3D) {
+        lhs = Point3D(x: lhs.x * rhs.x,
+                      y: lhs.y * rhs.y,
+                      z: lhs.z * rhs.z)
+    }
     
     static func / (lhs: Point3D, rhs: Size3D) -> Point3D {
         Point3D(x: lhs.x / rhs.width,
                 y: lhs.y / rhs.height,
                 z: lhs.z / rhs.depth)
+    }
+    
+    static func /= (lhs: inout Point3D, rhs: Point3D) {
+        lhs = Point3D(x: lhs.x / rhs.x,
+                      y: lhs.y / rhs.y,
+                      z: lhs.z / rhs.z)
     }
 }
