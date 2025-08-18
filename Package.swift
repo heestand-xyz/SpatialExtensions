5a7432b470bf88c48579dfd45bb3cd105fdba749
Anton Heestand <anton.heestand@gmail.com>
2025-08-18 21:07:41 +0900

    Adds Platforms

--- Package.swift
+++ Package.swift
@@ -6,8 +6,10 @@ let package = Package(
     name: "SpatialExtensions",
     platforms: [
         .iOS(.v16),
+        .tvOS(.v16),
         .macOS(.v13),
-        .visionOS(.v1)
+        .watchOS(.v9),
+        .visionOS(.v1),
     ],
     products: [
         .library(
