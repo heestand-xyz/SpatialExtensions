29ba4f5181d25507498ebea65c4b97de3ccd86a6
Anton Heestand <anton.heestand@gmail.com>
2025-08-19 12:34:50 +0900

    3D Dimension Type Updates

--- Sources/SpatialExtensions/Rect3D.swift
+++ Sources/SpatialExtensions/Rect3D.swift
@@ -1,6 +1,29 @@
 import Spatial
+import CoreGraphics
 
 public extension Rect3D {
     
     static let one = Rect3D(origin: .zero, size: .one)
+}
+
+public extension Rect3D {
+    
+    mutating func pad(_ padding: CGFloat) {
+        self = padded(padding)
+    }
+    
+    func padded(_ padding: CGFloat) -> Rect3D {
+        Rect3D(
+            origin: Point3D(
+                x: origin.x - padding,
+                y: origin.y - padding,
+                z: origin.z - padding
+            ),
+            size: Size3D(
+                width: size.width + padding * 2,
+                height: size.height + padding * 2,
+                depth: size.depth + padding * 2
+            )
+        )
+    }
 }
