d04fcd3dadc407feefbd2bddd275c6d394227506
Anton Heestand <anton.heestand@gmail.com>
2026-09-08 17:54:39 +0900

    Padding

--- Sources/SpatialExtensions/Rect3D.swift
+++ Sources/SpatialExtensions/Rect3D.swift
@@ -66,16 +66,22 @@ public extension Rect3D {
     
     func padded(_ padding: CGFloat) -> Rect3D {
         Rect3D(
-            origin: Point3D(
-                x: origin.x - padding,
-                y: origin.y - padding,
-                z: origin.z - padding
-            ),
-            size: Size3D(
-                width: size.width + padding * 2,
-                height: size.height + padding * 2,
-                depth: size.depth + padding * 2
-            )
+            origin: origin - padding,
+            size: size + padding * 2
+        )
+    }
+}
+
+public extension Rect3D {
+    
+    mutating func pad(_ padding: Size3D) {
+        self = padded(padding)
+    }
+    
+    func padded(_ padding: Size3D) -> Rect3D {
+        Rect3D(
+            origin: origin - padding,
+            size: size + padding * 2
         )
     }
 }
